77c2ca85a23da5a6e5ec163d1509f9c2537c6d1c
SamBritt <sam.britt2@yahoo.com>
2019-06-10 14:16:04 -0500

    Displaying all animals. Added funcitonality to add Grazing animals to GrazingAnimals List

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -4,22 +4,49 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseStock {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Cow");
-            Console.WriteLine ("2. Ostrich");
+namespace Trestlebridge.Actions
+{
+    public class PurchaseStock
+    {
+        public static void CollectInput(Farm farm)
+        {
+            Console.WriteLine("1. Chicken");
+            Console.WriteLine("2. Cow");
+            Console.WriteLine("3. Duck");
+            Console.WriteLine("4. Goat");
+            Console.WriteLine("5. Ostrich");
+            Console.WriteLine("6. Pig");
+            Console.WriteLine("7. Sheep");
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
 
             switch (Int32.Parse(choice))
             {
                 case 1:
+                    Console.WriteLine("Chickens do not graze, you fool!");
+                    break;
+                case 2:
                     ChooseGrazingField.CollectInput(farm, new Cow());
+                    break;
+                case 3:
+                    Console.WriteLine("Ducks do not graze, you fool!");
+                    break;
+                case 4:
+                    ChooseGrazingField.CollectInput(farm, new Goat());
+                    break;
+                case 5:
+                    ChooseGrazingField.CollectInput(farm, new Ostrich());
+                    break;
+                case 6:
+                    ChooseGrazingField.CollectInput(farm, new Pig());
+                    break;
+                case 7:
+                    ChooseGrazingField.CollectInput(farm, new Sheep());
                     break;
                 default:
                     break;
