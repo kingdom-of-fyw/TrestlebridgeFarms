571e2314b5dfd7f43e7200d7563a5737c8302595
Chris Morgan <bluemorgan2323@gmail.com>
2019-06-11 12:04:11 -0500

    Modified PurchaseStocks.cs so you can't input a number out of range or a letter

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -10,49 +10,78 @@ namespace Trestlebridge.Actions
     {
         public static void CollectInput(Farm farm)
         {
-            Console.WriteLine("1. Chicken");
-            Console.WriteLine("2. Cow");
-            Console.WriteLine("3. Duck");
-            Console.WriteLine("4. Goat");
-            Console.WriteLine("5. Ostrich");
-            Console.WriteLine("6. Pig");
-            Console.WriteLine("7. Sheep");
+            Console.WriteLine(@"1. Chicken");
+            Console.WriteLine(@"2. Cow");
+            Console.WriteLine(@"3. Duck");
+            Console.WriteLine(@"4. Goat");
+            Console.WriteLine(@"5. Ostrich");
+            Console.WriteLine(@"6. Pig");
+            Console.WriteLine(@"7. Sheep");
 
 
             Console.WriteLine();
             Console.WriteLine("What are you buying today?");
 
             Console.Write("> ");
-            string choice = Console.ReadLine();
 
-            switch (Int32.Parse(choice))
+            int choice = 0;
+            object stock = null;
+
+            while (choice == 0 && stock == null)
             {
-                case 1:
-                    ChooseChickenHouse.CollectInput(farm, new Chicken());
-                    Console.WriteLine("Chickens do not graze, you fool!");
-                    break;
-                case 2:
-                    ChooseGrazingField.CollectInput(farm, new Cow());
-                    break;
-                case 3:
-                    ChooseDuckHouse.CollectInput(farm, new Duck());
-                    Console.WriteLine("Ducks do not graze, you fool!");
-                    break;
-                case 4:
-                    ChooseGrazingField.CollectInput(farm, new Goat());
-                    break;
-                case 5:
-                    ChooseGrazingField.CollectInput(farm, new Ostrich());
-                    break;
-                case 6:
-                    ChooseGrazingField.CollectInput(farm, new Pig());
-                    break;
-                case 7:
-                    ChooseGrazingField.CollectInput(farm, new Sheep());
-                    break;
-                default:
-                    break;
+                try
+                {
+                    choice = Int32.Parse(Console.ReadLine());
+
+
+                    switch (choice)
+                    {
+                        case 1:
+                            ChooseChickenHouse.CollectInput(farm, new Chicken());
+                            Console.WriteLine("Chickens do not graze, you fool!");
+                            stock = new Chicken();
+                            break;
+                        case 2:
+                            ChooseGrazingField.CollectInput(farm, new Cow());
+                            stock = new Cow();
+                            break;
+                        case 3:
+                            ChooseDuckHouse.CollectInput(farm, new Duck());
+                            Console.WriteLine("Ducks do not graze, you fool!");
+                            stock = new Duck();
+                            break;
+                        case 4:
+                            ChooseGrazingField.CollectInput(farm, new Goat());
+                            stock = new Goat();
+                            break;
+                        case 5:
+                            ChooseGrazingField.CollectInput(farm, new Ostrich());
+                            stock = new Ostrich();
+                            break;
+                        case 6:
+                            ChooseGrazingField.CollectInput(farm, new Pig());
+                            stock = new Pig();
+                            break;
+                        case 7:
+                            ChooseGrazingField.CollectInput(farm, new Sheep());
+                            stock = new Chicken();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid selection. Click enter to return to the main menu.");
+                            Console.Read();
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please input a number from above!");
+                    Console.Write("> ");
+                }
             }
+
+            
+
+            
         }
     }
 }
